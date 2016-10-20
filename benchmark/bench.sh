1cb510eca9d019d52f211f698875297eba96a96b
Mike Lubinets <lubinetsm@yandex.ru>
2016-10-21 01:09:59 +0300

    [benchmark] config_* in .tvs

--- benchmark/bench.sh
+++ benchmark/bench.sh
@@ -11,16 +11,16 @@ while [[ $THREADS -le 128 ]]; do
     diesel migration run 
     popd
 
-    ab -n $COUNT -c $THREADS -g tests/${THREADS}_put.tsv -u data/put.json $SERVER/news
+    ab -n $COUNT -c $THREADS -g tests/${1}_${THREADS}_put.tsv -u data/put.json $SERVER/news
     sleep $TIMEOUT
-    ab -n $COUNT -c $THREADS -g tests/${THREADS}_get.tsv -r $SERVER/news/1
+    ab -n $COUNT -c $THREADS -g tests/${1}_${THREADS}_get.tsv -r $SERVER/news/1
     sleep $TIMEOUT
-    ab -n $COUNT -c $THREADS -g tests/${THREADS}_post.tsv -p data/post.json $SERVER/news
+    ab -n $COUNT -c $THREADS -g tests/${1}_${THREADS}_post.tsv -p data/post.json $SERVER/news
     sleep $TIMEOUT
 
-    cat template.plot | sed "s/THREADS/${THREADS}/g" | sed 's/METHOD/put/g' | gnuplot
-    cat template.plot | sed "s/THREADS/${THREADS}/g" | sed 's/METHOD/get/g' | gnuplot
-    cat template.plot | sed "s/THREADS/${THREADS}/g" | sed 's/METHOD/post/g' | gnuplot
+    #cat template.plot | sed "s/THREADS/${THREADS}/g" | sed 's/METHOD/put/g' | gnuplot
+    #cat template.plot | sed "s/THREADS/${THREADS}/g" | sed 's/METHOD/get/g' | gnuplot
+    #cat template.plot | sed "s/THREADS/${THREADS}/g" | sed 's/METHOD/post/g' | gnuplot
     
     THREADS=$((THREADS * 2))
 done
